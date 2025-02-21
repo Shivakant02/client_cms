91404f7033997913e9a7b8030623a15af2458f58
Shivakant <kants6397@gmail.com>
2025-02-22 00:55:35 +0530

    add light, dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,18 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
 import { Toaster } from 'react-hot-toast'
+import { ThemeProvider } from './contaxt/ThemeProvider.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+    <ThemeProvider>
     <BrowserRouter>
     <App />
     <Toaster/>
     </BrowserRouter>
+    </ThemeProvider>
     </Provider>
   </StrictMode>,
 )
